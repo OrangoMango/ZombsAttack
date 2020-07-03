8ef2c973f6a0a4a93bf6f3c82ab9abffb29cd603
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-07-03 16:54:47 +0200

    Update maps.py

--- maps.py
+++ maps.py
@@ -8,6 +8,8 @@ class MiniZombie:
         def draw(self, x, y):
                 #nx, ny = int(x/5) if x != 1 else int(x/5), int(y/5) if y != 1 else int(y/5)
                 self.minimap.canvas.move(self.id, x/5, y/5)
+        def delete(self):
+                self.minimap.canvas.delete(self.id)
 
 class MiniMap:
         def __init__(self, game):
@@ -15,5 +17,4 @@ class MiniMap:
                 self.canvas = Canvas(self.game.tk, width=100, height=100, bg="white")
                 self.canvas.grid(column=1, row=0)
                 self.playersign = self.canvas.create_oval(200/5, 200/5, 200/5+50/5, 200/5+50/5, fill="red")
-                self.zombssigns = []
-               # MiniZombie(self, 100, 100).draw(1, 1)
+                # MiniZombie(self, 100, 100).draw(1, 1)
