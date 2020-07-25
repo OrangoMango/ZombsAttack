eef1398ab610fc526101dd47f2d26dcb7e4d9f8d
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-07-25 20:40:10 +0200

    Add files via upload

--- version.py
+++ version.py
@@ -1,15 +1,23 @@
 import urllib.request, threading, time
 
 from tkinter import *
-
+import tkinter.ttk as t
 
 def th(ins):
     tk = Tk()
     tk.title(ins.window.profile.language_texts[36])
-    Label(tk, text=ins.window.profile.language_texts[36]).pack()
+    Label(tk, text=ins.window.profile.language_texts[36]+"...").pack()
+    p = t.Progressbar(tk, mode="indeterminate")
+    p.pack()
+    v = 0
     tk.update()
     while not ins.s:
-        pass
+        v += 20
+        if v >= 100:
+            v = 0
+        p.config(value=v)
+        tk.update()
+        time.sleep(0.1)
     tk.destroy()
 
 class Version:
@@ -55,17 +63,25 @@ class Version:
             d.append(x[0])
         return d
     def check(self):
-        if True: #self.get_current_version() < max(self.get_tags()): Temporaneamente
+        if True: #self.get_current_version() < max(self.get_tags()):
             return True
         else:
             return False
     def show_gui(self):
+        if False: #self.get_current_version() < max(self.get_tags()):
+            return
         tk = Tk()
-        tk.title(self.window.profile.language_texts[35])
+        tk.title(self.window.profile.language_texts[37])
         
         def focus(e):
             time.sleep(0.15)
             tk.focus_force()
-            
+
+        def later():
+            tk.destroy()
+        
+        oldv, newv = self.get_current_version(), max(self.get_tags())
         tk.bind("<FocusOut>", focus)
-        Label(tk, text=self.window.profile.language_texts[35]).pack()
+        Label(tk, text=self.window.profile.language_texts[35].format(newv), font="Calibri 12 bold", fg="blue").pack()
+        Button(tk, text=self.window.profile.language_texts[38].format(newv), font="Calibri 10 bold").pack()
+        Button(tk, text=self.window.profile.language_texts[39], command=later, font="Calibri 10 bold").pack()
