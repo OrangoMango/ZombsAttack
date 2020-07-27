c4cdda0b923846be3ec8bd2a8479347d6b6806f4
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-07-27 20:24:02 +0200

    Add files via upload

--- home.py
+++ home.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
-import time, webbrowser, os
+from tkinter import filedialog
+import time, webbrowser, os, sys, pickle, json, shutil
 
-import profiles, main
+import profiles, main, version
 
 class ScreenButton:
         def __init__(self, window):
@@ -14,6 +15,100 @@ class ScreenButton:
                 self.bg = self.window.canvas.create_rectangle(0, 0, 500, 300, fill="yellow")
                 self.backbutton = BackButton(self.window)
                 self.backbutton.toback.append(self.bg)
+
+class ProfileButton(ScreenButton):
+        def __init__(self, *args, name="Guest"):
+                ScreenButton.__init__(self, *args)
+                self.name = name
+                self.id = self.window.canvas.create_text(400, 20, text=self.name, font="Calibri 12")
+                self.window.canvas.tag_bind(self.id, "<Button-1>", self.click)
+        def click(self, event):
+                ScreenButton.click(self, event)
+                self.frame = LabelFrame(self.window.tk, text=self.window.profile.language_texts[20], bg="yellow")
+                wf = self.window.canvas.create_window(110, 10, window=self.frame, anchor="nw")
+                Label(self.frame, text=self.window.profile.language_texts[21]+": {0}".format(self.name), bg="yellow").grid()
+                Label(self.frame, text=self.window.profile.language_texts[22]+":", bg="yellow").grid(row=1)
+                Button(self.frame, text=self.window.profile.language_texts[23], command=self.download_profile, bg="red").grid(column=1, row=1)
+                self.backbutton.toback.append(wf)
+                ############################################
+                self.frame2 = LabelFrame(self.window.tk, text=self.window.profile.language_texts[25], bg="yellow")
+                wf2 = self.window.canvas.create_window(110, 90, window=self.frame2, anchor="nw")
+                self.backbutton.toback.append(wf2)
+                profiles_available = []
+                for f in os.listdir("Profiles"):
+                        profiles_available.append(f)
+                radiobuttons = []
+                self.select = StringVar(master=self.window.tk)
+                self.select.set(self.name)
+
+                def update_text_button():
+                        self.selection_button.config(text=self.window.profile.language_texts[26]+" \"{0}\"".format(self.select.get()))
+                        self.deletion_button.config(text=self.window.profile.language_texts[30]+" \"{0}\"".format(self.select.get()))
+                
+                for profile in profiles_available:
+                        r = Radiobutton(self.frame2, text=profile, variable=self.select, value=profile, command=update_text_button, bg="yellow4")
+                        r.grid(row=profiles_available.index(profile))
+                        radiobuttons.append(r)
+                self.selection_button = Button(self.frame2, text=self.window.profile.language_texts[26]+" \"{0}\"".format(self.select.get()), bg="red", command=self.select_profile)
+                self.selection_button.grid(row=len(profiles_available))
+                self.deletion_button = Button(self.frame2, text=self.window.profile.language_texts[30]+" \"{0}\"".format(self.select.get()), bg="red", command=self.delete_profile)
+                self.deletion_button.grid(row=len(profiles_available), column=1)
+                Button(self.frame2, text=self.window.profile.language_texts[27], bg="red", command=self.create_profile).grid(row=len(profiles_available)+1)
+                Button(self.frame2, text=self.window.profile.language_texts[29], bg="red", command=self.upload_profile).grid(row=len(profiles_available)+1, column=1)
+        def create_profile(self):
+                os.remove("profile.txt")
+                self.window.tk.destroy()
+                main.main()
+        def delete_profile(self):
+                ask = messagebox.askyesno(self.window.profile.language_texts[20], self.window.profile.language_texts[31])
+                if not ask:
+                        return
+                todelete = self.select.get()
+                if todelete == self.name:
+                        messagebox.showerror(self.window.profile.language_texts[20], self.window.profile.language_texts[33])
+                        return
+                os.remove("Profiles/"+todelete+"/data.json")
+                os.rmdir("Profiles/"+todelete)
+                messagebox.showinfo(self.window.profile.language_texts[20], self.window.profile.language_texts[32])
+                self.window.tk.destroy()
+                main.main()
+        def create_manual_profile(self, name, data=None):
+                os.mkdir("Profiles/"+name)
+                with open("Profiles/"+name+"/data.json", "w") as f:
+                        json.dump({"Trophies" : 0, "Brains" : 0, "Name" : name} if data is None else data, f) # Data RESET
+                        f.close()
+        def select_profile(self, name=None):
+                with open("profile.txt", "w") as f:
+                        f.write(self.select.get() if name is None else name)
+                        f.close()
+                n = self.select.get() if name is None else name
+                messagebox.showinfo(self.window.profile.language_texts[20], self.window.profile.language_texts[28]+": \""+ n +"\"")
+                self.window.tk.destroy()
+                main.main()
+        def download_profile(self):
+                path = filedialog.asksaveasfilename(filetypes=[(".bin Bin File", "*.bin")], initialfile="profile_{0}".format(self.name), initialdir="Bin_downloads")
+                if not path:
+                        return
+                f = open(path, "wb")
+                pickle.dump(self.window.profile.data, f)
+                messagebox.showinfo(self.window.profile.language_texts[20], self.window.profile.language_texts[24])
+                f.close()
+        def upload_profile(self):
+                path = filedialog.askopenfilename(filetypes=[(".bin Bin File", "*.bin")], initialdir="Bin_downloads")
+                if not path:
+                        return
+                f = open(path, "rb")
+                d = pickle.load(f)
+                if d["Name"] in os.listdir():
+                        ask = messagebox.askyesno(self.window.profile.language_texts[20], self.window.profile.language_texts[34])
+                        if not ask:
+                                return
+                        else:
+                                os.remove(d["Name"]+"/data.json")
+                                os.rmdir(d["Name"])
+                self.create_manual_profile(d["Name"], d)
+                self.select_profile(name=d["Name"])
+                f.close()
 
 class BackButton(ScreenButton):
         def __init__(self, *args):
@@ -84,7 +179,7 @@ class LanguageSelectButton:
                 messagebox.showinfo(self.window.profile.language_texts[8], self.window.profile.language_texts[9].format(self.language))
                 os.remove("language.txt")
                 self.window.profile.LANGUAGE = self.language
-                self.window.profile.config_name()
+                self.window.profile.load_languages()
                 self.window.tk.destroy()
                 main.main()
 
@@ -111,14 +206,24 @@ class LanguageButton(ScreenButton):
 
 class Window:
         def __init__(self):
+                self.version = 5.0
                 self.profile = profiles.Profile(self)
-                self.profile.set_asset()
+                try:
+                        self.profile.set_asset()
+                except Exception as e:
+                        print(e)
+                        messagebox.showerror("Error", "Internet Error, please verify your connection!")
+                        shutil.rmtree("../.zombsAttack")
+                        sys.exit()
                 self.tk = Tk()
                 self.tk.resizable(0, 0)
                 self.tk.title("ZombsAttack Lobby - OrangoMangoGames")
+                self.version_instance = version.Version(self)
+                self.version_instance.get_data()
+                self.check_update = self.version_instance.check()
                 self.canvas = Canvas(self.tk, width=500, height=300, bg="yellow")
                 self.canvas.pack()
-                self.canvas.create_text(3, 285, font="Calibri 6 bold", anchor="nw", text="Game made by OrangoMango (Paul Kocian, SCRIPT) and Dado14 (Andrea Pintus, IMAGES) (C) 2020")
+                self.canvas.create_text(3, 285, font="Calibri 6 bold", anchor="nw", text="Game made by OrangoMango (Paul Kocian, SCRIPT) and Dado14 (Andrea Pintus, DESIGN) v{0} (C) 2020".format(self.version))
                 self.playbutton = PlayButton(self)
                 self.helpbutton = HelpButton(self)
                 self.languagebutton = LanguageButton(self)
@@ -130,6 +235,8 @@ class Window:
                 self.canvas.tag_bind(self.helpbutton.id, "<Button-1>", self.helpbutton.click)
                 self.canvas.tag_bind(self.languagebutton.id, "<Button-1>", self.languagebutton.click)
                 self.profile.show_gui()
+                if self.check_update:
+                        self.version_instance.show_gui()
                 self.go = False
         def start(self, event):
                 self.tk.quit()
--- profiles.py
+++ profiles.py
@@ -3,6 +3,7 @@ import tkinter.ttk as t
 import tkinter.simpledialog as s
 
 import os, requests, json, platform
+import home
 
 class Profile:
         def __init__(self, home):
@@ -13,11 +14,13 @@ class Profile:
                 #self.path = "C:/Users/bambini/"
                 os.chdir(self.path)
                 self.name = ""
-                self.data = {"Trophies" : 0, "Brains" : 0}
+                self.data = {"Trophies" : 0, "Brains" : 0, "Name" : self.name}
                 self.language_texts = []
                 self.LANGUAGE = "english"
+                if not os.path.exists(".zombsAttack/Profiles"):
+                        os.mkdir(".zombsAttack/Profiles")
         def get_leagues(self):
-                l1, l2, l3, l4, l5, l6, l7 = self.language_texts[12:19]
+                l1, l2, l3, l4, l5, l6, l7, l8 = self.language_texts[12:20]
                 t = self.data["Trophies"]
 
                 # l1 t < 400
@@ -25,17 +28,40 @@ class Profile:
                 # l3 t >= 1000 and t < 1500
                 # l4 t >= 1500 and t < 2000
                 # l5 t >= 2000 and t < 3000
-                # l6 t >= 3000 and t < 4500
-                # l7 t >= 4500
+                # l6 t >= 3000 and t < 5000
+                # l7 t >= 5000 and t < 5800
+                # l8 t >= 5800
                 
                 self.leagues = { l1 : t < 400,
                                  l2 : t >= 400 and t < 800,
                                  l3 : t >= 1000 and t < 1500,
                                  l4 : t >= 1500 and t < 2000,
                                  l5 : t >= 2000 and t < 3000,
-                                 l6 : t >= 3000 and t < 4500,
-                                 l7 : t >= 4500
+                                 l6 : t >= 3000 and t < 5000,
+                                 l7 : t >= 5000 and t < 5800,
+                                 l8 : t >= 5800
                                 }
+
+                # l1 1
+                # l2 5
+                # l3 10
+                # l4 20
+                # l5 25
+                # l6 30
+                # l7 35
+                # l8 40
+
+                self.mtrophies = {
+                                   l1 : 1,
+                                   l2 : 5,
+                                   l3 : 10,
+                                   l4 : 20,
+                                   l5 : 25,
+                                   l6 : 30,
+                                   l7 : 35,
+                                   l8 : 40
+                                }
+                
                 league = None
                 for k, v in self.leagues.items():
                         if v:
@@ -44,60 +70,83 @@ class Profile:
                 return league
                 
         def load_languages(self):
-                if not os.path.exists("Data/Languages"):
-                        os.mkdir("Data/Languages")
-                        for lanfile in ["italiano", "english"]:
-                                r = requests.get("https://github.com/OrangoMango/ZombsAttack/raw/master/Data/Languages/{0}.txt".format(lanfile))
-                                open("Data/Languages/{0}.txt".format(lanfile), "wb").write(r.content)
-
-                LANGUAGE = self.LANGUAGE ####### SELECT HERE LANGUAGE ########
-                
-                with open("Data/Languages/{0}.txt".format(LANGUAGE)) as f:
-                        l = f.readlines()
-                        for i in l:
-                                self.language_texts.append(i.rstrip("\n"))
-                print("Sei in lega", self.get_leagues())
-                
-        def show_gui(self):
-                self.id = self.home.canvas.create_text(400, 10, text=self.name)
-                self.id_x = None #TBD
-                self.troph_id = self.home.canvas.create_text(30, 10, anchor="nw", text=self.data["Trophies"], font="Calibri 12 bold")
-                self.troph_id_x = self.home.canvas.create_rectangle(25, 5, 135, 35)
-                self.brains_id = self.home.canvas.create_text(160, 10, anchor="nw", text=self.data["Brains"], font="Calibri 12 bold")
-                self.brains_id_x = self.home.canvas.create_rectangle(155, 5, 265, 35)
-                self.league = self.home.canvas.create_rectangle(100, 5, 130, 35, fill="orange")
-        def ask_name(self):
-                while self.name == "" or self.name == None:
-                        self.name = s.askstring(self.language_texts[1], self.language_texts[1]+":")
-                print(self.name)
-        def create_profile_data(self):
-                with open(self.name+"/"+"data.json", "w") as d:
-                         json.dump(self.data, d, indent=4)
-        def load_saves(self):
-                if os.path.exists(self.name+"/"+"data.json"):
-                        with open(self.name+"/"+"data.json") as f:
-                                self.data = json.load(f)
-                #print(self.data) this prints current data dictionary
-        def save_saves(self):
-                        if os.path.exists(self.name+"/"+"data.json"):
-                                        with open(self.name+"/"+"data.json", "w") as f:
-                                                        json.dump(self.data, f, indent=4)
-        def config_name(self):
-                if os.path.exists("profile.txt"):
-                        with open("profile.txt") as f:
-                                self.name = f.readline()
-                else:
-                        with open("profile.txt", "w") as f:
-                                f.write(self.name)
                 if os.path.exists("language.txt"):
                         with open("language.txt") as f:
                                 self.LANGUAGE = f.readline()
                 else:
                         with open("language.txt", "w") as f:
                                 f.write(self.LANGUAGE)
+                f.close()
+                if not os.path.exists("Data/Languages"):
+                        os.mkdir("Data/Languages")
+                        for lanfile in ["italiano", "english"]:
+                                r = requests.get("https://github.com/OrangoMango/ZombsAttack/raw/master/Data/Languages/{0}.txt".format(lanfile))
+                                open("Data/Languages/{0}.txt".format(lanfile), "wb").write(r.content)
+
+                LANGUAGE = self.LANGUAGE
+                
+                with open("Data/Languages/{0}.txt".format(LANGUAGE)) as f:
+                        l = f.readlines()
+                        for i in l:
+                                self.language_texts.append(i.rstrip("\n"))
+                self.current_league = self.get_leagues()
+                
+        def show_gui(self):
+                self.id = home.ProfileButton(self.home, name=self.name).id
+                self.id_x = None #TBD
+                self.troph_id = self.home.canvas.create_text(30, 10, anchor="nw", text=self.data["Trophies"], font="Calibri 12 bold")
+                self.troph_id_x = self.home.canvas.create_rectangle(25, 5, 135, 35)
+                self.brains_id = self.home.canvas.create_text(160, 10, anchor="nw", text=self.data["Brains"], font="Calibri 12 bold")
+                self.brains_id_x = self.home.canvas.create_rectangle(155, 5, 265, 35)
+                self.league = self.home.canvas.create_rectangle(100, 5, 130, 35, fill="orange")
+        def ask_name(self, firsttime=False):
+                print(self.language_texts)
+                n = self.name
+                while self.name == "" or self.name == None or self.name in os.listdir():
+                        if firsttime:
+                                tk = Tk()
+                                tk.title("ZombsAttack")
+                                img = PhotoImage(file="Loadingimage.gif")
+                                Label(tk, image=img).pack()
+                        self.name = s.askstring(self.language_texts[1], self.language_texts[1]+":")
+                        if firsttime:
+                                tk.destroy()
+                self.data["Name"] = self.name
+                return n
+        def create_profile_data(self):
+                with open("Profiles/"+self.name+"/"+"data.json", "w") as d:
+                         json.dump(self.data, d, indent=4)
+        def load_saves(self):
+                if os.path.exists("Profiles/"+self.name+"/"+"data.json"):
+                        with open("Profiles/"+self.name+"/"+"data.json") as f:
+                                self.data = json.load(f)
+                #print(self.data) this prints current data dictionary
+        def save_saves(self):
+                        if os.path.exists("Profiles/"+self.name+"/"+"data.json"):
+                                        with open("Profiles/"+self.name+"/"+"data.json", "w") as f:
+                                                        json.dump(self.data, f, indent=4)
+        def config_name(self, ft=False):
+                if os.path.exists("profile.txt"):
+                        with open("profile.txt") as f:
+                                self.name = f.readline()
+                else:
+                        with open("profile.txt", "w") as f:
+                                n = self.ask_name(ft)
+                                f.write(self.name)
+                                if n in os.listdir():
+                                        return
+                                else:
+                                        self.create_profile_dir()
+                f.close()
+                if not os.path.exists("version.txt"):
+                        with open("version.txt", "w") as f:
+                                f.write(str(self.home.version))
+                                f.close()
+                if not os.path.exists("Bin_downloads"):
+                        os.mkdir("Bin_downloads")
         def create_profile_dir(self):
-                if not os.path.exists(self.name):
-                        os.mkdir(self.name)
+                if not os.path.exists("Profiles/"+self.name):
+                        os.mkdir("Profiles/"+self.name)
                         self.create_profile_data()
         def set_asset(self):
                 if not os.path.exists(".zombsAttack"):
@@ -123,20 +172,18 @@ class Profile:
                                         os.mkdir("Data/Images")
                                 img = ["Map", "Minimap", "Player", "Player_E", "Player_N", "Player_W"] + ["Zombie_{0}".format(x) for x in range(1, 9)] + ["PlayButton", "HelpButton", "Language", "Settings", "Statistics", "Back", "Shop"]
                                 for image in img:
-                                        l.config(text="Downloading {0}... {1}%".format(image+".gif", int(100/len(img)*done)))
                                         r = requests.get("https://github.com/OrangoMango/ZombsAttack/raw/master/Data/Images/{0}.gif".format(image))
                                         done += 1
                                         p.config(value=100/len(img)*done)
+                                        l.config(text="Downloading {0}... {1}%".format(image+".gif", int(100/len(img)*done)))
                                         tk.update()
                                         open("Data/Images/{0}.gif".format(image), "wb").write(r.content)
                         
-                        self.ask_name()
                         self.config_name()
-                        self.create_profile_dir()
                                         
                         tk.destroy()
                 else:
                         os.chdir(self.path+".zombsAttack")
-                        self.config_name()
-                        self.load_saves()
                         self.load_languages()
+                        self.config_name(True)
+                        self.load_saves()
